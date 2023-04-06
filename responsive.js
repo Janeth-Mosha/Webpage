aed357dbd3800cf795671ed334336ad090c784ef
Janeth-Mosha <janethmosha937@gmail.com>
2023-04-06 04:19:08 -0700

    create a webpage
    first commit of my webpage

---
+++ responsive.js
@@ -0,0 +1,8 @@
+let hamburger = document.querySelector(".hambuger");
+
+hamburger.addEventListener("click", function(){
+    let navBar = document.querySelector(".nav-bar");
+        navBar = document.querySelector(".nav-bar");
+        navBar.classList.toggle("active");
+        console.log(navBar.classList);
+})
